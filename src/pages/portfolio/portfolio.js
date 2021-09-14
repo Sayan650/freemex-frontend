4478da393d58b977394d28b27f15079e1d64ef44
Amool-kk <amoolkumar10@gmail.com>
2021-09-14 04:26:53 -0400

    portfolio maked responsive

--- src/pages/portfolio/portfolio.js
+++ src/pages/portfolio/portfolio.js
@@ -1,9 +1,31 @@
 import "./portfolio.css"
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import React, { useState } from 'react'
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
 
 
 const Portfolio = () => {
+
+    const [BUYmodal, setbuyModal] = useState(false)
+    const [sellmodal, setsellModal] = useState(false)
+
+    const openModal = (e) => {
+        if (e.target.innerHTML === "Buy More") {
+            setbuyModal(true)
+        }if (e.target.innerHTML === "Sell") {
+            setsellModal(true)
+        }
+    }
+    const closeModal = (e) => {
+        setbuyModal(false)
+        setsellModal(false)
+    }
+
+
     return (
-        <div className="foliocont">
+        <div className="portfolio">
             <div className="heading">
                 <h1>Portfolio</h1>
             </div>
@@ -29,130 +51,59 @@ const Portfolio = () => {
                             <p className="noStocks">
                                 x3
                             </p>
-                            <div className="priceStocks">$ 148.97 <span>-5.10</span></div>
+                            <div className="priceStocks">$ 148.97 <span><ArrowDownwardIcon className="downIcon" /> -5.10</span></div>
                             <div className="updateStocks">Last Update: <span>Sep 11th, 1:30:00 am </span> <span className="span">-3.31%</span>
                             </div>
                             <div className="button">
-                                <button className="buymore">Buy More</button>
-                                <button className="sell">Sell</button>
-                            </div>
-                        </div>
-                        <div className="scard">
-                            <p className="nameStock">AAPL</p>
-                            <p className="nStock"><span>Apple inc.</span></p>
-                            <p className="noStocks">
-                                x3
-                            </p>
-                            <div className="priceStocks">$ 148.97 <span>-5.10</span></div>
-                            <div className="updateStocks">Last Update: <span>Sep 11th, 1:30:00 am </span> <span className="span">-3.31%</span>
-                            </div>
-                            <div className="button">
-                                <button className="buymore">Buy More</button>
-                                <button className="sell">Sell</button>
-                            </div>
-                        </div>
-                        <div className="scard">
-                            <p className="nameStock">AAPL</p>
-                            <p className="nStock"><span>Apple inc.</span></p>
-                            <p className="noStocks">
-                                x3
-                            </p>
-                            <div className="priceStocks">$ 148.97 <span>-5.10</span></div>
-                            <div className="updateStocks">Last Update: <span>Sep 11th, 1:30:00 am </span> <span className="span">-3.31%</span>
-                            </div>
-                            <div className="button">
-                                <button className="buymore">Buy More</button>
-                                <button className="sell">Sell</button>
-                            </div>
-                        </div>
-                        <div className="scard">
-                            <p className="nameStock">AAPL</p>
-                            <p className="nStock"><span>Apple inc.</span></p>
-                            <p className="noStocks">
-                                x3
-                            </p>
-                            <div className="priceStocks">$ 148.97 <span>-5.10</span></div>
-                            <div className="updateStocks">Last Update: <span>Sep 11th, 1:30:00 am </span> <span className="span">-3.31%</span>
-                            </div>
-                            <div className="button">
-                                <button className="buymore">Buy More</button>
-                                <button className="sell">Sell</button>
-                            </div>
-                        </div>
-                        <div className="scard">
-                            <p className="nameStock">AAPL</p>
-                            <p className="nStock"><span>Apple inc.</span></p>
-                            <p className="noStocks">
-                                x3
-                            </p>
-                            <div className="priceStocks">$ 148.97 <span>-5.10</span></div>
-                            <div className="updateStocks">Last Update: <span>Sep 11th, 1:30:00 am </span> <span className="span">-3.31%</span>
-                            </div>
-                            <div className="button">
-                                <button className="buymore">Buy More</button>
-                                <button className="sell">Sell</button>
-                            </div>
-                        </div>
-                        <div className="scard">
-                            <p className="nameStock">AAPL</p>
-                            <p className="nStock"><span>Apple inc.</span></p>
-                            <p className="noStocks">
-                                x3
-                            </p>
-                            <div className="priceStocks">$ 148.97 <span>-5.10</span></div>
-                            <div className="updateStocks">Last Update: <span>Sep 11th, 1:30:00 am </span> <span className="span">-3.31%</span>
-                            </div>
-                            <div className="button">
-                                <button className="buymore">Buy More</button>
-                                <button className="sell">Sell</button>
-                            </div>
-                        </div>
-                        <div className="scard">
-                            <p className="nameStock">AAPL</p>
-                            <p className="nStock"><span>Apple inc.</span></p>
-                            <p className="noStocks">
-                                x3
-                            </p>
-                            <div className="priceStocks">$ 148.97 <span>-5.10</span></div>
-                            <div className="updateStocks">Last Update: <span>Sep 11th, 1:30:00 am </span> <span className="span">-3.31%</span>
-                            </div>
-                            <div className="button">
-                                <button className="buymore">Buy More</button>
-                                <button className="sell">Sell</button>
-                            </div>
-                        </div>
-                        <div className="scard">
-                            <p className="nameStock">AAPL</p>
-                            <p className="nStock"><span>Apple inc.</span></p>
-                            <p className="noStocks">
-                                x3
-                            </p>
-                            <div className="priceStocks">$ 148.97 <span>-5.10</span></div>
-                            <div className="updateStocks">Last Update: <span>Sep 11th, 1:30:00 am </span> <span className="span">-3.31%</span>
-                            </div>
-                            <div className="button">
-                                <button className="buymore">Buy More</button>
-                                <button className="sell">Sell</button>
-                            </div>
-                        </div>
-                        <div className="scard">
-                            <p className="nameStock">AAPL</p>
-                            <p className="nStock"><span>Apple inc.</span></p>
-                            <p className="noStocks">
-                                x3
-                            </p>
-                            <div className="priceStocks">$ 148.97 <span>-5.10</span></div>
-                            <div className="updateStocks">Last Update: <span>Sep 11th, 1:30:00 am </span> <span className="span">-3.31%</span>
-                            </div>
-                            <div className="button">
-                                <button className="buymore">Buy More</button>
-                                <button className="sell">Sell</button>
+                                <button className="buymore" onClick={(e)=>openModal(e)}>Buy More</button>
+                                <button className="sell" onClick={(e)=>openModal(e)}>Sell</button>
                             </div>
                         </div>
 
+
                     </div>
                 </div>
+                <div className="market">
+                    <button className="marketbtn">Buy Stocks</button>
+                </div>
             </div>
+
+
+            {/* -------------- modal for buy More ------------- */}
+            
+            <Modal style={{ width: 'fit-content' }} isOpen={BUYmodal} onRequestClose={() => setbuyModal(false)}>
+                <div className="modalheader">
+                    <h1>Buy : Apple Inc.</h1>
+                </div>
+                <hr />
+                <div className="modalBody">
+                    <p>Max you can buy  : 3333</p>
+                    <input type="text" placeholder="Quantity" />
+                </div>
+                <hr />
+                <div className="modalFooter">
+                    <button className="buy">Buy</button>
+                    <button className="close" onClick={(e)=>closeModal(e)} >Cancel</button>
+                </div>
+            </Modal>
+
+            {/* -------------- modal for sell ------------- */}
+
+
+            <Modal style={{ width: 'fit-content' }} isOpen={sellmodal} onRequestClose={() => setsellModal(false)}>
+                <div className="modalheader">
+                    <h1>Sell : Apple Inc.</h1>
+                </div>
+                <hr />
+                <div className="modalBody">
+                    <input type="number"  />
+                </div>
+                <hr />
+                <div className="modalFooter">
+                    <button className="buy">Sell</button>
+                    <button className="close" onClick={(e)=>closeModal(e)} >Cancel</button>
+                </div>
+            </Modal>
         </div>
     )
 }
